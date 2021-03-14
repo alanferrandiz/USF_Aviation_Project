3e0163fae1bafe186651e08539180ed24a86e6d0
Alan Ferrándiz Langley <alanferrandiz@msn.com>
2021-03-14 18:05:36 -0400

    First commit

--- usf_aviation_inserts_06_CABIN_TYPES.sql
+++
@@ -1,6 +0,0 @@
---INSERT TABLE [cabin_types]
-
-INSERT [dbo].[cabin_types] ([cabin_type_id], [name]) VALUES (1, N'First Class')
-GO
-INSERT [dbo].[cabin_types] ([cabin_type_id], [name]) VALUES (2, N'Second Class')
-GO
