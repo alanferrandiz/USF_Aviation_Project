036b61873104f14c58b00ff267dc719489e76e9b
Alan Ferrándiz Langley <alanferrandiz@msn.com>
2021-03-14 16:42:39 -0400

    First commit

--- usf_aviation_inserts_05_LOCATION_TYPES.sql
+++ usf_aviation_inserts_05_LOCATION_TYPES.sql
@@ -2,12 +2,7 @@
 
 INSERT [dbo].[location_types] ([location_type_id], [name]) VALUES (1, N'Automated Sales Kiosk')
 GO
-INSERT [dbo].[location_types] ([location_type_id], [name]) VALUES (2, N'Airport Station')
+INSERT [dbo].[location_types] ([location_type_id], [name]) VALUES (2, N'Customer Service Office')
 GO
-INSERT [dbo].[location_types] ([location_type_id], [name]) VALUES (3, N'Customer Service Office')
+INSERT [dbo].[location_types] ([location_type_id], [name]) VALUES (3, N'Web or App')
 GO
-INSERT [dbo].[location_types] ([location_type_id], [name]) VALUES (4, N'Web site')
-GO
-INSERT [dbo].[location_types] ([location_type_id], [name]) VALUES (5, N'iOS App')
-GO
-INSERT [dbo].[location_types] ([location_type_id], [name]) VALUES (6, N'Android App')
